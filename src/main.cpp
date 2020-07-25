3b8cd3d92a16e415845e0db7cfcdfbea7cab3586
msperkow <sperkowski.mikolaj@gmail.com>
2020-07-25 13:50:53 +0200

    #1: Update gitignore; make it so that during build cmake copies proper libraries; add install to cmake; fix winmain error during compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "controller/application.hpp"
+#include <iostream>
 
-// Main code
+#ifdef _WIN32
+#include <minwindef.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow)
+#else
 int main(int argc, char** argv)
+#endif
 {
+	std::cout << "elo" << std::endl;
 	Application application(DEFAULT_WINDOW_TITLE);
 	application.run();
 	return 0;
