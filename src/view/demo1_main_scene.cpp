03e3836842957632b9ad2972a584f7f672953d47
msperkow <sperkowski.mikolaj@gmail.com>
2020-10-21 17:28:31 +0200

    Fix bug introduced while merging

--- src/view/demo1_main_scene.cpp
+++ src/view/demo1_main_scene.cpp
@@ -15,6 +15,8 @@ Demo1MainScene::Demo1MainScene(Application* application, Window* window,
 	setCameraPosition(glm::vec3(0.0f, .5f, 0.0f));
 
 	light = std::make_unique<Lightsource>(glm::vec3(4.0f, 1.0f, 10.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	terrain = std::make_unique<Mesh>(std::vector<Vertex>(), std::vector<unsigned int>(), GL_STATIC_DRAW, glm::vec3(.376f, .502f, .22f), light.get());
+	updateTerrain();
 
 	frames.emplace_back(std::make_unique<TerrainControlFrame>(this, "Generation Control", modelManipulator));
 	frames.emplace_back(std::make_unique<CameraControlFrame>(this, "Camera Control"));
--- src/view/terrain_models_view.cpp
+++ src/view/terrain_models_view.cpp
@@ -3,8 +3,6 @@
 TerrainModelsView::TerrainModelsView(Application *application, Window *window, TerrainTranslator *terrainTranslator)
 : CameraView(application, window), terrainTranslator(terrainTranslator)
 {
-	terrain = std::make_unique<Mesh>(std::vector<Vertex>(), std::vector<unsigned int>(), GL_STATIC_DRAW);
-	updateTerrain();
 }
 
 void TerrainModelsView::draw()
